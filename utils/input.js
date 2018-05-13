d0d6e795af93f0666c7229789cad4ca987c981e3
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-05-12 19:06:54 -0700

    remove input.js

--- utils/input.js
+++ utils/input.js
@@ -1,23 +1,23 @@
-const axios = require('axios');
-const merge = require('lodash/merge');
-const readline = require('readline');
-
-const Plop = require('../models/plop');
-const getPlebIds = require('./crawler');
-const localhost = 'http://localhost:3001';
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
-});
-
-let max = 50000;
-let count = 0;
-
-async function addPlebsToPlop(masterPlopId, ADDED = {}) {
-  
-}
-
-console.log('Enter plopname: ');
-rl.on('line', addPlebsToPlop)
+// const axios = require('axios');
+// const merge = require('lodash/merge');
+// const readline = require('readline');
+//
+// const Plop = require('../models/plop');
+// const getPlebIds = require('./crawler');
+// const localhost = 'http://localhost:3001';
+//
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+//   terminal: false
+// });
+//
+// let max = 50000;
+// let count = 0;
+//
+// async function addPlebsToPlop(masterPlopId, ADDED = {}) {
+//
+// }
+//
+// console.log('Enter plopname: ');
+// rl.on('line', addPlebsToPlop)
